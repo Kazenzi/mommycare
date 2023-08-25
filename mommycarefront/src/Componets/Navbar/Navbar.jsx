23ca978a76d7f63de9c0b033944abe2b3cdea520
Kazenzi <jacintakazenzi99@gmail.com>
2023-08-25 18:06:32 +0300

    nav

--- mommycarefront/src/Componets/Navbar/Navbar.jsx
+++ mommycarefront/src/Componets/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { ImSpinner9 } from "react-icons/im";
 import{PiDotsNineBold} from "react-icons/pi";
 import{AiOutlineCloseCircle} from "react-icons/ai"
+
+
 const Navbar = () => {
+
+  // trackandupdatenavbar
+  const[navBar,setNavBar]=useState("menu");
+  // functiontoshownavbar
+  const showNavBar=()=>{
+    setNavBar("menu showNavbar");
+  }
   return (
     <div className='navBar'>
        <div className="logoDiv">
        <ImSpinner9 className="icon"/>
        <span>Mommy Care</span>
        </div>
-       <div className="menu">
+       <div className="navBar">
         <ul>
             <li className='navlist'>Nutrition tips</li>
             <li className='navlist'>Emergency</li>
@@ -19,13 +28,12 @@ const Navbar = () => {
             <li className='navlist'>Chat</li>
         </ul>
         {/* removenav */}
-        <AiOutlineCloseCircle className="icon"/>
+        <AiOutlineCloseCircle className="icon closeIcon"/>
        </div>
        <button className='chatBtn btn'>
-        Chat
-        <PiDotsNineBold className="icon"/>
-
-       </button>
+        Chat Us
+             </button>
+       <PiDotsNineBold className="icon menuIcon" onClick={showNavBar}/>
        </div>
        
   )
